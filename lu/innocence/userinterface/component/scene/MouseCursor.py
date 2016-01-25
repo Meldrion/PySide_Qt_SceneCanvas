c8c37869f177a75dfa9d04442c81a0ad0fd62951
Meldrion <meldrion@gmail.com>
2016-01-26 00:01:48 +0100

    update

---
+++ lu/innocence/userinterface/component/scene/MouseCursor.py
@@ -0,0 +1,39 @@
+from PySide import QtCore, QtGui
+
+
+class MouseCursor(QtGui.QGraphicsItem):
+    def __init__(self, unit_size, parent=None):
+        super(MouseCursor, self).__init__(self)
+        self.unit_start_x = 0
+        self.unit_start_y = 0
+        self.unit_selection_width = 0
+        self.unit_selection_height = 0
+        self.current_tileset = None
+
+    def paint(self, painter, option, widget):
+
+        rect = self.boundingRect()
+        if self.current_tileset is not None:
+            painter.setOpacity(0.45);
+
+            for i in range(self.unit_start_X, self.unit_start_X + self.unit_selection_width):
+                for j in range(self.unit_start_Y, self.unit_start_Y + self.unit_selection_width):
+
+                    if self.m_current_tileset.isInRange(i, j):
+                        painter.drawPixmap((i - self.unit_start_X) * self.unit_size,
+                                           (j - self.unit_start_Y) * self.unit_size,
+                                           self.unit_size, self.unit_size,
+                                           self.current_tileset.getTileAt(i, j))
+
+        painter.fillRect(rect, QtGui.QBrush(self.rect_color))
+
+    def setSelectionStart(self, selection_start_x, selection_start_y):
+        self.unit_start_x = selection_start_x
+        self.unit_start_y = selection_start_y
+
+    def setSelectionDimension(self, unit_selection_width, unit_selection_height):
+        self.unit_selection_width = unit_selection_width
+        self.unit_selection_height = unit_selection_height
+
+    def setCurrentTileset(self, tileset):
+        self.current_tileset = tileset
--- lu/innocence/userinterface/component/scene/MouseCursorLayer.py
+++ lu/innocence/userinterface/component/scene/MouseCursorLayer.py
@@ -1,7 +1,50 @@
-from PySide import QtGui
+from PySide import QtGui, QtCore
 
 from lu.innocence.userinterface.component.scene.AbstractLayer import AbstractLayer
+from lu.innocence.userinterface.component.scene.MouseCursor import MouseCursor
 
 
 class MouseCursorLayer(AbstractLayer):
-    pass
+    def __init__(self, unit_width, unit_height, unit_size, parent=None):
+        self.mouse_is_down = False
+        self.current_layer_index = 0
+        self.current_tileset = None
+        self.current_selection_rect = None
+        self.mouseCursor = MouseCursor(unit_size, self)
+        self.mouse_unit_position = QtCore.QPointF(0, 0)
+        self.mouseCursor.setSelectionStart(0, 10)
+        self.mouseCursor.setSelectionDimension(5, 5)
+        self.hide()
+
+    def paint(painter, option, widget):
+        pass
+
+    def mouseMoveEvent(self, event):
+        scenePos = event.scenePos()
+        if self.boundingRect().contains(scenePos):
+
+            self.mouseCursor.show()
+            unit_pos = QtGui.QPointF(scenePos.x() / self.unit_size, scenePos.y() / self.unit_size)
+
+            if unit_pos.x() != self.mouse_unit_position.x() or unit_pos.y() != self.mouse_unit_position.y():
+                self.mouse_unit_position = QtCore.QPointF(unit_pos)
+                self.mouseCursor.setPos(self.mouse_unit_position.x() * self.unit_size,
+                                        self.mouse_unit_position.y() * self.unit_size)
+
+        else:
+            self.mouseCursor.hide()
+
+    def mousePressEvent(self, event):
+        self.mouse_is_down = True
+
+    def mouseReleaseEvent(self, event):
+        self.mouse_is_down = False
+
+    def setCurrentTileset(self, tileset):
+        self.current_tileset = tileset
+        self.mouseCursor.setCurrentTileset(tileset)
+
+    def setTilesetSelection(self, rect):
+        self.current_selection_rect = QtCore.QRectF(rect)
+        self.mouseCursor.setSelectionStart(rect.x(), rect.y())
+        self.mouseCursor.setSelectionDimension(rect.width(), rect.height())
